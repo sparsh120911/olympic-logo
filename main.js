44a9289ffbb317a9351d660cafa1267bb7c88d10
sparsh120911 <83496970+sparsh120911@users.noreply.github.com>
2021-10-02 09:37:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function canvas()
+{
+    canvas-
+    document.getElementById("myCanvas");
+    ctx= canvas.getContext("2d");
+
+    ctx.beginPath();
+    ctx.strokeStyle="blue";
+    ctx.lineWidth = 5;
+    ctx.arc(250,210,40,0,2 * Math.PI);
+    ctx.stroke(); 
+    
+    ctx.beginPath();
+    ctx.strokeStyle="blue";
+    ctx.lineWidth = 5;
+    ctx.arc(250,210,40,0,2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle="blue";
+    ctx.lineWidth = 5;
+    ctx.arc(250,210,40,0,2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle="blue";
+    ctx.lineWidth = 5;
+    ctx.arc(250,210,40,0,2 * Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.strokeStyle="blue";
+    ctx.lineWidth = 5;
+    ctx.arc(250,210,40,0,2 * Math.PI);
+    ctx.stroke();
+}
